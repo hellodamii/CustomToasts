e41f1dc010bb76fce8da3eefe29f1521f8feadaf
hellodamii <daremiarchs@gmail.com>
2024-08-19 22:09:48 +0100

    Initial Commit

---
+++ CustomToasts/CustomToastsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomToastsApp.swift
+//  CustomToasts
+//
+//  Created by Busha on 19/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomToastsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
